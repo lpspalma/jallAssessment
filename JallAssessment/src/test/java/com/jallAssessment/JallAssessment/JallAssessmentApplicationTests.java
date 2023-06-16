c83f4dc98be7e2f09984422f25d74160d1bd32a9
Lucas Palma <lu.palma.silva@gmail.com>
2023-06-16 08:55:54 -0300

    main project

---
+++ JallAssessment/src/test/java/com/jallAssessment/JallAssessment/JallAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jallAssessment.JallAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JallAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
